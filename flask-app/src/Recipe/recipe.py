b3304407bf1a5e5f54b483c7563843930896a399
EthanTheFanger <ethanby2@gmail.com>
2024-04-22 14:59:59 -0700

    api changes

--- flask-app/src/Recipe/recipe.py
+++ flask-app/src/Recipe/recipe.py
@@ -5,19 +5,17 @@ from src import db
 recipe = Blueprint('recipe', __name__)
 
 #define route
-@recipe.route("/difficulty", methods=['GET'])
+@recipe.route("/", methods=["GET"])
 
-#select all recipes that take less time then the provided amount of time
-def max_difficulty():
+#gets all the recipes
+def get_recipes():
     cursor = db.get_db().cursor()
-    least = request.args.get('least')
-
-    steps = cursor.execute("SELECT * FROM Recipes WHERE {least} > SELECT COUNT(*) FROM recipes WHERE LOWER(Steps) LIKE '%step%' ")
-    cursor.close()
-
+    cursor.execute("SELECT * FROM Recipe")
+    
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
+
     for row in theData:
         json_data.append(dict(zip(row_headers, row)))
     the_response = make_response(jsonify(json_data))
@@ -25,9 +23,26 @@ def max_difficulty():
     the_response.mimetype = 'application/json'
     return the_response
 
-#specify route
-@recipe.route("/excludeingredients", methods=['GET'])
 
-def withoutingredients():
-    return 0
-    
+#define route
+@recipe.route("/difficulty", methods=['GET'])
+
+#select all recipes that take less steps then the provided amount of steps
+def max_difficulty():
+    cursor = db.get_db().cursor()
+    least = request.args.to_dict().get('least')
+
+    steps = cursor.execute("SELECT * FROM Recipe WHERE LENGTH(column_name) - LENGTH(REPLACE(column_name, 'Step', '')) < {lesat};".format(least)) 
+    cursor.close()
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
