9fe167ef5ec8dcb68ccd238e7881a5bb47f02169
EthanTheFanger <ethanby2@gmail.com>
2024-04-21 23:31:53 -0700

    added recipe apis

---
+++ flask-app/src/Recipe/recipe.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+recipe = Blueprint('recipe', __name__)
+
+#define route
+@recipe.route("/", methods=['GET','POST'])
+
+#select all recipes that take less time then the provided amount of time
+def max_difficulty():
+    cursor = db.get_db().cursor()
+    least = request.args.get('least')
+
+    steps = cursor.execute("SELECT * FROM Recipes WHERE {least} > SELECT COUNT(*) FROM recipes WHERE LOWER(Steps) LIKE '%step%' ")
+    cursor.close()
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+def exclude_ingredient():
+    cursor = db.get_db().cursor() 
+    excluded = request.args.get('excluded')
+
+    ingredients = cursor.execute()
