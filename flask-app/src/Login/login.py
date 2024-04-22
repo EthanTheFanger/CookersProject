1e5fd1da446082e504c9a6d5bee01c6db07dcd58
Brian Yang <brianyang1206@gmail.com>
2024-04-22 01:00:06 -0400

    login api fixes

--- flask-app/src/Login/login.py
+++ flask-app/src/Login/login.py
@@ -4,19 +4,18 @@ from src import db
 
 login = Blueprint('login', __name__,)
 
-#connect cursor
-cursor = db.get_db().cursor()
-
 #define route
-@login.route("/login", methods = ['GET'])
+@login.route("/", methods = ['GET'])
 
 def check():
+    cursor = db.get_db().cursor()
     username = request.args.get('username')
     password = request.args.get('password')
 
     #checks if the login and password are correct and in the database
-    cursor.execute("SELECT Username FROM login WHERE Username={0} AND Password={1}".format(username,password))
-    
+    cursor.execute("SELECT Username FROM Login WHERE Username='{0}' AND Password='{1}'".format(username, password))
+    cursor.close()
+
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
