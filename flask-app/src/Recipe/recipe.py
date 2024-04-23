d1df7fd6e30e714360b228d462327f3cb7ebcdd3
EthanTheFanger <ethanby2@gmail.com>
2024-04-22 17:04:40 -0700

    more recipe API changes

--- flask-app/src/Recipe/recipe.py
+++ flask-app/src/Recipe/recipe.py
@@ -30,19 +30,38 @@ def get_recipes():
 #select all recipes that take less steps then the provided amount of steps
 def max_difficulty():
     cursor = db.get_db().cursor()
-    least = request.args.to_dict().get('least')
+    most = request.args.to_dict().get('most')
 
-    steps = cursor.execute("SELECT * FROM Recipe WHERE LENGTH(column_name) - LENGTH(REPLACE(column_name, 'Step', '')) < {lesat};".format(least)) 
+    recipes = cursor.execute("SELECT * FROM Recipe WHERE (LENGTH(Steps) - LENGTH(REPLACE(Steps, 'Step', '')))/4 <= %s;", (most)) 
+    result = cursor.fetchall()
     cursor.close()
 
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
+    return jsonify({'result': result}), 200
 
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+#define function
+@recipe.route("/include", methods=["GET"])
+
+#Returns a list of recipes that include the provided list of ingredients
+def include_ingredients():
+    cursor = db.get_db().cursor()
+    ingredients = request.args.to_dict().get('ingredients')
+
+    cursor.execute("SELECT * FROM Recipe JOIN Ingredients ON Recipe.RecipeID = Ingredients.RecipeID WHERE Ingredients.Ingredient NOT IN {ingredients};", {ingredients})
+
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify({'result': result}), 200
+
+@recipe.route("/exclude", methods=["GET"])
+
+#returns a list of recipes that exclude the rpovided list of ingredients
+def exclude_ingredients():
+    cursor = db.get_db().cursor()
+    ingredients = request.args.to_dict().get('ingredients')
+
+    cursor.execute("SELECT * FROM Recipe JOIN Ingredients ON Recipe.RecipeID = Ingredients.RecipeID WHERE Ingredients.Ingredient IN {ingredients};", {ingredients})
+
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify({'result': result}), 200
 
